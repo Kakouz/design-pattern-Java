fe569e7fc26c34e8ca6ddc4e9e39bb6be23df454
Tiago Prudente <tf.prudente@gmail.com>
2023-03-25 14:07:23 -0300

    Template Method

---
+++ src/design_patterns/behavioral/template_method/networks/Network.java
@@ -0,0 +1,24 @@
+package design_patterns.behavioral.template_method.networks;
+
+public abstract class Network {
+	String userName;
+	String password;
+
+	Network() {
+	}
+
+	public boolean post(String message) {
+		if (logIn(this.userName, this.password)) {
+			boolean result = sendData(message.getBytes());
+			logOut();
+			return result;
+		}
+		return false;
+	}
+
+	abstract boolean logIn(String userName, String password);
+
+	abstract boolean sendData(byte[] data);
+
+	abstract void logOut();
+}
