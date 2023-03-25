62cf7e9590cbd39c4bb4729001a24dd994bfd3a6
Tiago Prudente <tf.prudente@gmail.com>
2023-03-25 14:31:27 -0300

    Visitor

---
+++ src/design_patterns/behavioral/visitor/visitor/Visitor.java
@@ -0,0 +1,16 @@
+package design_patterns.behavioral.visitor.visitor;
+
+import design_patterns.behavioral.visitor.shapes.Dot;
+import design_patterns.behavioral.visitor.shapes.Circle;
+import design_patterns.behavioral.visitor.shapes.CompoundShape;
+import design_patterns.behavioral.visitor.shapes.Rectangle;
+
+public interface Visitor {
+	String visitDot(Dot dot);
+
+	String visitCircle(Circle circle);
+
+	String visitRectangle(Rectangle rectangle);
+
+	String visitCompoundGraphic(CompoundShape cg);
+}
